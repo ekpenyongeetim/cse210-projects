ceafd884728e88516993f259f5702999b5d0b6e5
ekpenyongeetim <ekpenyongeetim@gmail.com>
2024-05-04 23:58:08 +0100

    leraning 02 added job description of a personnel

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creating job instances
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        // Creating a resume instance
+        Resume myResume = new Resume("Allison Rose");
+
+        // Adding jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Displaying resume details
+        myResume.Display();
     }
 }
